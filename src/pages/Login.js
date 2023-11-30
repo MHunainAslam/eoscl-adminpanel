8e9f0e22d826c19d39adb383ad5f4247e0315381
mhunainaslam <hunainaslam.ha@gmail.com>
2023-12-01 03:49:21 +0500

    .

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import back from '../assets/images/Feature-Image-How-to-Successfully-Sell-on-Amazon-This-Holiday-Season.jpg'
+import back2 from '../assets/images/61fa370baa75264321c10ccf_cricket.jpg'
 import logo from '../assets/images/logo/eoscl-logo.png'
 import LoginForm from '../components/auth/LoginForm'
 
@@ -11,7 +12,7 @@ const Login = () => {
                     <div className="row align-items-center min-vh-100 justify-content-center">
                         <div className="row col-md-10">
                             <div className="col-lg-6 d-lg-block d-none login-left">
-                                <img src={back} className='' alt="" />
+                                <img src={back2} className='' alt="" />
                             </div>
                             <div className="col-lg-6 my-auto login-right text-center">
                                 <img src={logo} className='' alt="" />
