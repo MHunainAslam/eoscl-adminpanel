e9dbbe1a38f37dad21b563338e9af70039fd8b58
mhunainaslam <hunainaslam.ha@gmail.com>
2023-12-28 03:45:03 +0500

    .

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import back from '../../assets/images/Feature-Image-How-to-Successfully-Sell-on-Amazon-This-Holiday-Season.jpg'
 import back2 from '../../assets/images/61fa370baa75264321c10ccf_cricket.jpg'
-import logo from '../../assets/images/logo/eoscl-logo.png'
+import logo from '../../assets/images/logo/logo.png'
 import LoginForm from '../../components/auth/LoginForm'
 
 const Login = ({ authme }) => {
